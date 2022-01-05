7597c8dd1f64dee39b33b18a55524170fbd2bb51
Denis Beschastnykh <beschastnykh.denis@gmail.com>
2022-01-05 22:53:58 +0500

    Refactor Progress and Scale tests

--- src/range-slider/View/subviews/Progress.spec.ts
+++ src/range-slider/View/subviews/Progress.spec.ts
@@ -4,13 +4,14 @@ describe('Progress class:', () => {
   document.body.innerHTML = '<div><div>';
   const [el] = document.body.children;
   const progress = new Progress(el as HTMLElement);
+  const newOptions = { from: 10, to: 20, visible: true };
 
   test('Can set and get options', () => {
-    const newOptions = { from: 10, to: 20, visible: true };
-
     progress.setOptions(newOptions);
     expect(progress.getOptions()).toMatchObject(newOptions);
+  });
 
+  test('Can update options', () => {
     progress.setOptions({ visible: false });
     expect(progress.getOptions()).toMatchObject({ ...newOptions, visible: false });
   });
--- src/range-slider/View/subviews/Scale.spec.ts
+++ src/range-slider/View/subviews/Scale.spec.ts
@@ -4,18 +4,19 @@ describe('Scale class:', () => {
   document.body.innerHTML = '<div><div>';
   const [el] = document.body.children;
   const scale = new Scale(el as HTMLElement);
+  const newOptions = {
+    min: 0,
+    max: 10,
+    step: 1,
+    visible: true,
+  };
 
   test('Can set and get options', () => {
-    const newOptions = {
-      min: 0,
-      max: 10,
-      step: 1,
-      visible: true,
-    };
-
     scale.setOptions(newOptions);
     expect(scale.getOptions()).toMatchObject(newOptions);
+  });
 
+  test('Can update options', () => {
     scale.setOptions({ visible: false });
     expect(scale.getOptions()).toMatchObject({ ...newOptions, visible: false });
   });
